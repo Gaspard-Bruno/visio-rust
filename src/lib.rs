f0e0a232b023737ee8a10acd902a8a004b9cbbae
Anderson <andycancado@gmail.com>
2021-11-17 08:47:39 +0000

    add get exif dict in python

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
 use pyo3::prelude::*;
 use pyo3::types::{PyBytes, PyDict};
+use std::collections::HashMap;
 use std::io::Cursor;
 
 #[pyfunction]
-fn getexif(bts: &[u8]) -> PyResult<()> {
+fn getexif(bts: &[u8]) -> PyResult<HashMap<String, String>> {
     let res = read_bytes(bts);
+    let mut exif = HashMap::new();
     match res {
         Ok(r) => {
             for f in r.fields() {
-                println!("{}: {}", f.tag, f.display_value().with_unit(&r));
+                exif.insert(f.tag.to_string(), f.display_value().to_string());
             }
         }
         Err(e) => {
@@ -16,19 +18,18 @@ fn getexif(bts: &[u8]) -> PyResult<()> {
             ()
         }
     }
-    Ok(())
+    Ok(exif)
 }
-
 
 fn read_metadata(bytes: &PyBytes) -> () {
     // TODO read metadata to return a dictionary
-    
+    todo!();
 }
 
 fn write_metadata(dict: PyDict, bytes: &PyBytes) -> () {
     // TODO write metadata and bytes to return bytes
+    todo!();
 }
-
 
 fn read_bytes(b: &[u8]) -> Result<exif::Exif, exif::Error> {
     let mut x = Cursor::new(b);
@@ -36,7 +37,6 @@ fn read_bytes(b: &[u8]) -> Result<exif::Exif, exif::Error> {
 
     Ok(exif)
 }
-
 
 #[pymodule]
 fn visio_exif(_py: Python, m: &PyModule) -> PyResult<()> {
