c41c0efe29c664b67c93f0337f8c9477a4b4db72
Marco João <marco.joao@gaspardbruno.com>
2021-12-17 17:45:46 +0000

    api definition

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,47 @@
 use pyo3::prelude::*;
-use pyo3::types::{PyBytes, PyDict};
-use std::collections::HashMap;
-use std::io::Cursor;
+use pyo3::types::PyDict;
 
-/// Get exif dictionary from image file
-#[pyfunction]
-#[pyo3(text_signature = "(bytes, /)")]
-fn getexif(bytes: &[u8]) -> PyResult<HashMap<String, String>> {
-    let res = read_bytes_from_container(bytes);
-    let mut exif = HashMap::new();
-    match res {
-        Ok(r) => {
-            for f in r.fields() {
-                exif.insert(f.tag.to_string(), f.display_value().to_string());
-            }
-        }
-        Err(e) => {
-            println!("ERROR {}", e);
-            ()
-        }
-    }
-    Ok(exif)
-}
+use img_parts::{DynImage, ImageEXIF, ImageICC};
 
-/// Read read_metadata from exif bytes
-#[pyfunction]
-#[pyo3(text_signature = "(bytes, /)")]
-fn read_metadata(_bytes: &PyBytes) -> () {
-    todo!("read metadata to return a dictionary");
-}
-
-/// Write exif bytes from dictionary
+/// get exif, icc profile from file bytes
 #[pyfunction]
 #[pyo3(text_signature = "(fields, bytes, /)")]
-fn write_metadata(_fields: &PyDict, _bytes: &PyBytes) -> () {
-    todo!("write metadata and bytes to return bytes");
+fn get_metadata_from_bytes(file_bytes: &[u8]) -> (&PyDict, &PyDict) {
+    //visio_exif.get_metadata_from_bytes(bytes) -> (exif, iccprofile)
+
+    todo!("get exif, icc profile from file bytes");
+}
+/// get exif, icc profile from file path
+#[pyfunction]
+#[pyo3(text_signature = "(fields, bytes, /)")]
+fn get_metadata_from_path(file_path: &str) -> (&PyDict, &PyDict) {
+    // visio_exif.get_metadata_from_path(path) -> (exif, iccprofile)
+
+    todo!("get exif, icc profile from file path");
 }
 
-/// Read bytes from container
-fn read_bytes_from_container(b: &[u8]) -> Result<exif::Exif, exif::Error> {
-    let mut x = Cursor::new(b);
-    let exif = exif::Reader::new().read_from_container(&mut x)?;
+/// set exif, icc
+#[pyfunction]
+#[pyo3(text_signature = "(fields, bytes, /)")]
+fn set_metadata_from_bytes(file_bytes: &[u8], meta_data: (&PyDict, &PyDict)) -> Vec<u8> {
+    // visio_exif.set_metadata_from_bytes(bytes, (exif, iccprofile)) -> bytes
 
-    Ok(exif)
+    todo!("set exif, icc");
+}
+/// set exif, icc
+#[pyfunction]
+#[pyo3(text_signature = "(fields, bytes, /)")]
+fn set_metadata_from_path(file_path: &[u8], meta_data: (&PyDict, &PyDict)) -> Vec<u8> {
+    // visio_exif.ser_metadata_from_path(path, (exif, iccprofile))  -> bytes
+
+    todo!("set exif, icc");
 }
 
 #[pymodule]
 fn visio_exif(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(getexif, m)?)?;
-    m.add_function(wrap_pyfunction!(read_metadata, m)?)?;
-    m.add_function(wrap_pyfunction!(write_metadata, m)?)?;
+    m.add_function(wrap_pyfunction!(get_metadata_from_bytes, m)?)?;
+    m.add_function(wrap_pyfunction!(get_metadata_from_path, m)?)?;
+    m.add_function(wrap_pyfunction!(set_metadata_from_bytes, m)?)?;
+    m.add_function(wrap_pyfunction!(set_metadata_from_path, m)?)?;
     Ok(())
 }
