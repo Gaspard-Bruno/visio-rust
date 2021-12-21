186bfc0cac9f9ccad56ce0d5dd8710c6575bb4aa
Marco João <marco.joao@gaspardbruno.com>
2021-12-21 19:23:20 +0000

    Implement an example of read and write metadata

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,52 @@
-use pyo3::prelude::*;
-use pyo3::types::PyDict;
+// use pyo3::prelude::*;
+// use pyo3::types::{PyByteArray, PyDict};
+// use std::io::Cursor;
 
-use img_parts::{DynImage, ImageEXIF, ImageICC};
+// use img_parts::{Bytes, DynImage, ImageEXIF, ImageICC};
 
-/// get exif, icc profile from file bytes
-#[pyfunction]
-#[pyo3(text_signature = "(fields, bytes, /)")]
-fn get_metadata_from_bytes(file_bytes: &[u8]) -> (&PyDict, &PyDict) {
-    //visio_exif.get_metadata_from_bytes(bytes) -> (exif, iccprofile)
+// /// get exif, icc profile from file bytes
+// #[pyfunction]
+// #[pyo3(text_signature = "(fields, bytes, /)")]
+// fn get_metadata_from_bytes(file_bytes: &[u8]) -> () {
+//     //visio_exif.get_metadata_from_bytes(bytes) -> (exif, iccprofile)
 
-    todo!("get exif, icc profile from file bytes");
-}
-/// get exif, icc profile from file path
-#[pyfunction]
-#[pyo3(text_signature = "(fields, bytes, /)")]
-fn get_metadata_from_path(file_path: &str) -> (&PyDict, &PyDict) {
-    // visio_exif.get_metadata_from_path(path) -> (exif, iccprofile)
+//     let (iccp, exif) = DynImage::from_bytes(file_bytes.clone().into())
+//         .expect("image loaded")
+//         .map_or((None, None), |dimg| (dimg.icc_profile(), dimg.exif()));
 
-    todo!("get exif, icc profile from file path");
-}
+//     ()
+// }
+// /// get exif, icc profile from file path
+// #[pyfunction]
+// #[pyo3(text_signature = "(fields, bytes, /)")]
+// fn get_metadata_from_path(file_path: &str) -> (&PyDict, &PyDict) {
+//     // visio_exif.get_metadata_from_path(path) -> (exif, iccprofile)
 
-/// set exif, icc
-#[pyfunction]
-#[pyo3(text_signature = "(fields, bytes, /)")]
-fn set_metadata_from_bytes(file_bytes: &[u8], meta_data: (&PyDict, &PyDict)) -> Vec<u8> {
-    // visio_exif.set_metadata_from_bytes(bytes, (exif, iccprofile)) -> bytes
+//     todo!("get exif, icc profile from file path");
+// }
 
-    todo!("set exif, icc");
-}
-/// set exif, icc
-#[pyfunction]
-#[pyo3(text_signature = "(fields, bytes, /)")]
-fn set_metadata_from_path(file_path: &[u8], meta_data: (&PyDict, &PyDict)) -> Vec<u8> {
-    // visio_exif.ser_metadata_from_path(path, (exif, iccprofile))  -> bytes
+// /// set exif, icc
+// #[pyfunction]
+// #[pyo3(text_signature = "(fields, bytes, /)")]
+// fn set_metadata_from_bytes(file_bytes: &[u8], meta_data: (&PyDict, &PyDict)) -> Vec<u8> {
+//     // visio_exif.set_metadata_from_bytes(bytes, (exif, iccprofile)) -> bytes
 
-    todo!("set exif, icc");
-}
+//     todo!("set exif, icc");
+// }
+// /// set exif, icc
+// #[pyfunction]
+// #[pyo3(text_signature = "(fields, bytes, /)")]
+// fn set_metadata_from_path(file_path: &[u8], meta_data: (&PyDict, &PyDict)) -> Vec<u8> {
+//     // visio_exif.ser_metadata_from_path(path, (exif, iccprofile))  -> bytes
 
-#[pymodule]
-fn visio_exif(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(get_metadata_from_bytes, m)?)?;
-    m.add_function(wrap_pyfunction!(get_metadata_from_path, m)?)?;
-    m.add_function(wrap_pyfunction!(set_metadata_from_bytes, m)?)?;
-    m.add_function(wrap_pyfunction!(set_metadata_from_path, m)?)?;
-    Ok(())
-}
+//     todo!("set exif, icc");
+// }
+
+// #[pymodule]
+// fn visio_exif(_py: Python, m: &PyModule) -> PyResult<()> {
+//     m.add_function(wrap_pyfunction!(get_metadata_from_bytes, m)?)?;
+//     m.add_function(wrap_pyfunction!(get_metadata_from_path, m)?)?;
+//     m.add_function(wrap_pyfunction!(set_metadata_from_bytes, m)?)?;
+//     m.add_function(wrap_pyfunction!(set_metadata_from_path, m)?)?;
+//     Ok(())
+// }
---
+++ src/main.rs
@@ -0,0 +1,93 @@
+use std::collections::HashMap;
+use std::env::args;
+use std::fs;
+
+use std::process::exit;
+use img_parts::{DynImage, ImageEXIF, ImageICC, Bytes};
+
+
+fn main() {
+    let mut args = args();
+    args.next();
+
+    let input_path = match args.next() {
+        Some(path) => path,
+        None => {
+            eprintln!("Please specify the input file path (must be a jpeg, png or webp)");
+            exit(1);
+        }
+    };
+
+    let input_buf = fs::read(input_path).expect("read input file");
+    
+    let nodata = Bytes::new().to_vec();
+    let meta = export_metadata(input_buf.clone());
+    let output_buf = import_metadata(input_buf.clone(), meta["ICC_PROFILE"].clone(), nodata.clone());
+    let meta = export_metadata(output_buf.clone());
+    //let output_buf
+    println!("DATA: {:?}", meta)
+}
+
+
+pub fn import_metadata(buf:Vec<u8>, icc_profile: Vec<u8>, exif_data: Vec<u8>) ->  Vec<u8> {
+    let original_image = buf.clone();
+    let bts = Bytes::from(buf);
+    let iccp = Bytes::from(icc_profile);
+    let exif = Bytes::from(exif_data);
+    let image = DynImage::from_bytes(bts).expect("image loaded");
+    if image.is_some() {
+        let mut out = image.unwrap();
+        out.set_icc_profile(iccp.into());
+        out.set_exif(exif.into());
+        let out_bts = out.encoder().bytes();
+        out_bts.to_vec()
+    } else {
+        original_image
+    }
+}
+
+pub fn export_metadata(buf: Vec<u8>) -> HashMap<&'static str, Vec<u8>> {
+    // extract ICC and EXIF metadata
+    let  (iccp, exif) = DynImage::from_bytes(buf.into())
+        .expect("image loaded")
+        .map_or((None, None), |dimg| (dimg.icc_profile(), dimg.exif()));
+        
+    let icc_profile = iccp.unwrap_or(Bytes::new()).to_vec();
+    let exif_data = exif.unwrap_or(Bytes::new()).to_vec();
+    let metadata = HashMap::from([
+        ("ICC_PROFILE", icc_profile),
+        ("EXIF_DATA", exif_data),
+    ]);
+    metadata
+}
+
+
+
+// /// saves the image to the specified `path`
+// ///
+// /// If `iccp` *and* `exif` are None, the image is written directly to `path`, else
+// /// it is buffered in memory, modified by img-parts and then written to `path`.
+// fn save(img: image::DynamicImage, path: &Path, iccp: Option<img_parts::Bytes>, exif: Option<img_parts::Bytes>) {
+//     let out_format = image::ImageFormat::from_path(&path).expect("detected output format");
+//     let mut out_file = File::create(path).expect("create output file");
+
+//     if iccp.is_some() || exif.is_some() {
+//         let mut out = BytesMut::new().writer();
+//         img.write_to(&mut out, out_format).expect("image encoded");
+//         let out = out.into_inner().freeze();
+
+//         match DynImage::from_bytes(out.clone()).expect("image loaded") {
+//             Some(mut dimg) => {
+//                 dimg.set_icc_profile(iccp);
+//                 dimg.set_exif(exif);
+//                 dimg.encoder()
+//                     .write_to(out_file)
+//                     .expect("output file written");
+//             }
+//             None => out_file.write_all(&out).expect("output file written"),
+//         };
+//     } else {
+//         img.write_to(&mut out_file, out_format)
+//             .expect("image encoded without ICCP or EXIF");
+//     }
+// }
