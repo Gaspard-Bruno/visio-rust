e7852a31312fab004faa6bd9205e8c8a750bd52e
Marco João <marco.joao@gaspardbruno.com>
2021-12-22 11:11:21 +0000

    general refactor

--- python/visio_exif/main.py
+++ python/visio_exif/main.py
@@ -3,9 +3,7 @@ import visio_img_meta
 with open('images/22-canon_tags.jpeg', 'rb') as f:
     input_data = f.read()
 
-    icc_profile, exif_data = visio_img_meta.export_metadata(input_data)
-    output_data = visio_img_meta.import_metadata(input_data, icc_profile, exif_data)
+    icc_profile, exif_data = visio_img_meta.get_metadata(input_data)
+    output_data = visio_img_meta.set_metadata(input_data, icc_profile, exif_data)
 
     print(input_data == output_data)
-    f.seek(0)
-    f.write(output_data)
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,27 @@
 use pyo3::prelude::*;
-use base64::encode;
 use img_parts::{Bytes, DynImage, ImageEXIF, ImageICC};
 
 #[pyfunction]
 #[pyo3(text_signature = "(buf, icc_profile, exif_data, /)")]
-pub fn import_metadata(buf:Vec<u8>, icc_profile: Vec<u8>, exif_data: Vec<u8>) -> String {
+pub fn set_metadata(buf:Vec<u8>, icc_profile: Vec<u8>, exif_data: Vec<u8>) -> Vec<u8> {
     match DynImage::from_bytes(buf.clone().into()).unwrap() {
         Some(mut img) => {
             img.set_icc_profile(Bytes::from(icc_profile).into());
             img.set_exif(Bytes::from(exif_data).into());
-            encode(img.encoder().bytes().to_vec()) 
+            img.encoder().bytes().to_vec()
         },
-        None => encode(buf)
+        None => buf
     }
 }
 
 #[pyfunction]
 #[pyo3(text_signature = "(buf, /)")]
-pub fn export_metadata(buf: Vec<u8>) -> (Vec<u8>, Vec<u8>) {
+pub fn get_metadata(buf: Vec<u8>) -> (Vec<u8>, Vec<u8>) {
 
     match DynImage::from_bytes(buf.into()).unwrap() {
         Some(img) => { 
-            let icc_profile = img.icc_profile().unwrap_or(Bytes::new()).to_vec();
-            let exif_data = img.exif().unwrap_or(Bytes::new()).to_vec();
+            let icc_profile = img.icc_profile().unwrap_or_default().to_vec();
+            let exif_data = img.exif().unwrap_or_default().to_vec();
             (icc_profile, exif_data)
         },
         None => (vec![], vec![])
@@ -32,7 +31,7 @@ pub fn export_metadata(buf: Vec<u8>) -> (Vec<u8>, Vec<u8>) {
 #[pymodule]
 #[pyo3(name = "visio_img_meta")]
 fn visio_exif(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(import_metadata, m)?)?;
-    m.add_function(wrap_pyfunction!(export_metadata, m)?)?;
+    m.add_function(wrap_pyfunction!(set_metadata, m)?)?;
+    m.add_function(wrap_pyfunction!(get_metadata, m)?)?;
     Ok(())
 }
