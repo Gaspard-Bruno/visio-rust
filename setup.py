cffec75fe67aa529f38c30cf2ad199347534e9ba
Marco João <marco.joao@gaspardbruno.com>
2021-12-23 15:03:17 +0000

    fix typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools_rust import Binding, RustExtension
 setup(
     name="visio_rust",
     version="0.1.2",
-    rust_extensions=[RustExtension("word_count.word_count", debug=False)],
+    rust_extensions=[RustExtension("visio_rust", debug=False)],
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
 )
