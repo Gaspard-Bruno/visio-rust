d8934492c6d2f964a9c0df89fc41446cdbd7a22a
Anderson <andycancado@gmail.com>
2021-12-23 19:06:08 +0000

    wip ignore exif

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,11 @@ use pyo3::prelude::*;
 
 #[pyfunction]
 #[pyo3(text_signature = "(buf, icc_profile, exif_data, /)")]
-pub fn set_metadata(buf: Vec<u8>, icc_profile: Vec<u8>, exif_data: Vec<u8>) -> Vec<u8> {
+pub fn set_metadata(buf: Vec<u8>, icc_profile: Vec<u8>, _exif_data: Vec<u8>) -> Vec<u8> {
     match DynImage::from_bytes(buf.clone().into()).unwrap() {
         Some(mut img) => {
+            //img.set_exif(Bytes::from(exif_data).into());
             img.set_icc_profile(Bytes::from(icc_profile).into());
-            img.set_exif(Bytes::from(exif_data).into());
             img.encoder().bytes().to_vec()
         }
         None => buf,
@@ -19,6 +19,7 @@ pub fn set_metadata(buf: Vec<u8>, icc_profile: Vec<u8>, exif_data: Vec<u8>) -> V
 pub fn get_metadata(buf: Vec<u8>) -> (Vec<u8>, Vec<u8>) {
     match DynImage::from_bytes(buf.into()).unwrap() {
         Some(img) => {
+            
             let icc_profile = img.icc_profile().unwrap_or_default().to_vec();
             let exif_data = img.exif().unwrap_or_default().to_vec();
             (icc_profile, exif_data)
