9cd7a02dfb9ff9c755f676fa11fc5d09e1a3b718
Marco João <marco.joao@gaspardbruno.com>
2021-12-23 14:54:04 +0000

    bump version v0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="visio_rust",
-    version="0.1.1",
+    version="0.1.2",
     rust_extensions=[RustExtension("word_count.word_count", debug=False)],
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
